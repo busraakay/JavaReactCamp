750ea8f89fcf29370491214255a1734b0e976d2b
Büşra Akay <61781887+busraakay@users.noreply.github.com>
2021-04-30 16:51:40 +0300

    3. hafta ödevi

---
+++ 3rdWeekHomework/src/thirdWeekHomework/Instructor.java
@@ -0,0 +1,19 @@
+package thirdWeekHomework;
+
+public class Instructor extends User {
+    private String coursesTaught;
+
+    public Instructor(int id, String firstName, String lastName, String Email, String coursesTaught){
+        super(id, firstName, lastName, Email);
+        this.coursesTaught = coursesTaught;
+    }
+
+
+    public String getCoursesTaught() {
+        return coursesTaught;
+    }
+
+    public void setCoursesTaught(String coursesTaught) {
+        this.coursesTaught = coursesTaught;
+    }
+}
